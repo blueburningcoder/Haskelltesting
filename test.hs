b4a7e23cd0fbdffc6b29a66ec7a76b6d8c9dbb69
blueburningcoder <f.karg10@gmail.com>
2015-10-05 17:34:23 +0200

    latest stuff

--- test.hs
+++ test.hs
@@ -1,3 +1,8 @@
+import Data.List
+import qualified Data.Set as Set
+
+
+
 doubleMe :: Int -> Int
 doubleMe x = x + x
 
@@ -18,6 +23,8 @@ doubleIfSmall x = if x < 100 then x ^ 2 else x + 1
 lastNum :: [Int]
 lastNum = [0,1,2,3,4,5,6,7,8,9] -- == [0..10]
 -- [(lastNum !! x) | x <- [0,1..(length lastNum -1)]]
+
+values = [-4.3, -2.4, -1.2, 0.4, 2.3, 5.9, 10.5, 29.1, 5.3, -2.4, -14.5, 2.9, 2.3]
 
 
 lucky :: (Integral a) => a -> String
@@ -106,6 +113,79 @@ zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
 zipWith' _ [] _ = []
 zipWith' _ _ [] = []
 zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' y ys = foldl (\acc x -> if x == y then True else acc) False ys
+
+-- import Data.List
+
+numUniques :: (Eq a) => [a] -> Int
+numUniques = length . nub
+
+-- span ~ takeWhile
+-- break = span (not . p)
+
+
+on :: (b -> b -> c) -> (a -> b) -> a -> a-> c
+on f g = \x y -> f (g x) (g y)
+-- from Data.Function
+
+
+findKey :: (Eq k) => k -> [(k,v)] -> Maybe v
+findKey _ [] = Nothing
+findKey key ((k,v):xs) = if k == key then Just v else findKey key xs
+
+-- findKey key  = foldr (\(k,v) acc -> if k == key then Just v else acc) Nothing
+
+
+text1 = "I just had an anime dream. Anime ... Reality ... Are they so different?"
+
+text2 = "The old man left his garbage can out and now his trash is all over my lawn!"
+
+
+
+
+-- module Shapes (Point(..), Shape(..), surface, nudge, baseCircle, baseRect) where
+
+data Point = Point Float Float deriving (Show)
+data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
+
+refPoint = Point 0 0
+
+surface :: Shape -> Float
+surface (Circle _ r) = pi * r ^ 2
+surface (Rectangle (Point x1 y1) (Point x2 y2) ) = (abs $ x2 - x1) * (abs $ y2 - y1)
+
+
+pointAdd :: Point -> Float -> Float -> Point
+pointAdd (Point x y) x2 y2 = Point (x + x2) (y + y2)
+
+nudge :: Shape -> Float -> Float -> Shape -- 'schubsen' -> to move
+nudge (Circle p r) a b = Circle (pointAdd p a b) r
+nudge (Rectangle p1 p2) a b = Rectangle (pointAdd p1 a b) (pointAdd p2 a b)
+
+baseCircle :: Float -> Shape
+baseCircle = Circle (Point 0 0)
+
+baseRect :: Float -> Float -> Shape
+baseRect w h = Rectangle (Point 0 0) (Point w h)
+
+
+
+
+
+
+
+data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday 
+           deriving (Eq, Ord, Show, Read, Bounded, Enum)
+
+
+infixr 5 :-:
+
+data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
+
+
 
 
 
