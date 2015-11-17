bd64f67e9407652e297c324d38e7548674c4c11a
blueburningcoder <f.karg10@gmail.com>
2015-11-17 07:56:51 +0100

    on train

--- workingGaussian.hs
+++ workingGaussian.hs
@@ -22,9 +22,29 @@ oneTheDiagonal = undefined
 toGMatrice :: Matrice -> Vector -> GMatrice
 toGMatrice = undefined
 
-zeroExceptDiagonal :: GMatrice -> GMatrice
-zeroExceptDiagonal m = foldf m b zeroThisValue
+-- zeros every value below the diagonal ( except the lowest line, as of yet)
+zeroBelowDiagonal :: GMatrice -> GMatrice
+zeroBelowDiagonal m = foldf m b zeroThisValue
 
+-- zeros only the lowest line 
+zeroLowestLine :: GMatrice -> GMatrice
+zeroLowestLine = undefined
+
+-- returns a 'fitting' line for the given line from the given Matrice
+getFittingLine :: GMatrice -> Line -> Line
+getFittingLine = undefined
+
+-- if the lines are 'fitting' in the sense of having zeros at the same places and are not equal
+lineFittingNotEqual :: Line -> Line -> Bool
+lineFittingNotEqual (Line vec d) (Line bec b) = (foldfit (d:vec) (b:bec) True) && vec /= bec
+
+-- folds over the vectors 
+foldfit :: Vector -> Vector -> Bool -> Bool
+foldfit [] _ b = b
+foldfit _ [] b = b
+foldfit (l:li) (x:xs) b = (foldfit li xs) (b == True && ( (l == 0.0 && x == 0.0) || (l /= 0.0 && x /= 0.0) ) )
+
+-- a fold, implemented due to the need for two variable coming out of the list at each time
 foldf :: GMatrice -> [[Int]] -> (GMatrice -> Int -> Int -> GMatrice) -> GMatrice
 foldf m [] _ = m
 foldf m ([x,y]:c) f
@@ -34,8 +54,8 @@ foldf m ([x,y]:c) f
         foldf m c f
 
 b = [[x,y] | x <- [0..3], y <- [0..3]] :: [[Int]]
--- Ending in 
 
+-- zeroing one explicit value and returning the new Matrice
 zeroThisValue :: GMatrice -> Int -> Int -> GMatrice
 zeroThisValue a@(GMatrice (b@(Line vec d):(c@(Line veC dd)):l)) x 0 = 
         let factor = calcZeroFactor (vec !! x) (veC !! x)
@@ -43,57 +63,59 @@ zeroThisValue a@(GMatrice (b@(Line vec d):(c@(Line veC dd)):l)) x 0 =
         in insGMat a newLine 0
 zeroThisValue (GMatrice (l:li)) x y = 
         let (GMatrice list) = zeroThisValue (GMatrice (li ++ [l])) x (y-1)
-        in GMatrice [last list] ++ (init list)
+        in GMatrice ([last list] ++ (init list))
 
+-- subtracting one line from the other
 lineSub :: Line -> Line -> Line
 lineSub (Line vec d) (Line bec b) = (Line (vecSub vec bec) (d - b))
 
+-- subtracting one Vector from the other
 vecSub :: Vector -> Vector -> Vector
 vecSub [] _ = []
 vecSub _ [] = []
 vecSub (v:vec) (b:bec) = (v - b):(vecSub vec bec)
 
+-- a -> b -> c => c * b - a = 0
+-- the substraicton of the multiplication of this factor with the second parameter with the first parameter is going to be zero -- is this description even correct ?
 calcZeroFactor :: Double -> Double -> Double
 calcZeroFactor d dd = d / dd 
 
-getZeroFactor :: Line -> Line -> Int -> Line
-getZeroFactor (Line vec d) (Line veC dd) i = undefined
-
-zeroBelowTheDiagonal :: GMatrice -> Int -> GMatrice
-zeroBelowTheDiagonal (GMatrice (l:li)) i = undefined
-
-fromGMatrice :: GMatrice -> (Matrice, Vector)
-fromGMatrice (GMatrice ((Line vec d):li)) = undefined
-
+-- inserts the given Line at the given offset in the given GMatrice
 insGMat :: GMatrice -> Line -> Int -> GMatrice
 insGMat (GMatrice (l:li)) nl i 
         | i == 0 = GMatrice (nl:li)
         | otherwise = insGMat (GMatrice li) nl (i - 1)
         
 
+-- might return the wanted line. Nothing if the GMatrice is empty
 getCol :: GMatrice -> Int -> Maybe Line
 getCol (GMatrice []) _ = Nothing
 getCol (GMatrice (l:li)) 0 = Just l
 getCol (GMatrice (l:li)) i = getCol (GMatrice li) (i-1)
 
+-- returns a vector of only the resulting values from the linear expression
 getVal :: GMatrice -> Vector
 getVal (GMatrice []) = []
 getVal (GMatrice ((Line vec d):li)) = d:(getVal (GMatrice li))
 
+-- doing an operation over the whole GMatrice
 gMatOp :: GMatrice -> (Double -> Double) -> GMatrice
 gMatOp (GMatrice []) _ = (GMatrice [])
 gMatOp (GMatrice (l:li)) f = GMatrice $ (lineOp l f):(getGMatrice $ gMatOp (GMatrice li) f)
 
+-- doing an operation on the given line
 lineOp :: Line -> (Double -> Double) -> Line
 lineOp (Line vec d) f = Line (vecOp vec f) (f d)
 
+-- doing an operation on one Line of the Matrice
 colOp :: GMatrice -> Int -> (Double -> Double) -> GMatrice
 colOp m 0 f = GMatrice $ (lineOp (head . getGMatrice $ m) f):(tail . getGMatrice $ m)
 colOp m i f = GMatrice $ (head . getGMatrice $ m):(getGMatrice (colOp (GMatrice (tail . getGMatrice $ m)) (i-1) f))
 
+-- doing an operation on the vector
 vecOp :: Vector -> (Double -> Double) -> Vector
 vecOp [] _ = []
 vecOp (v:vec) f = (f v):(vecOp vec f)
 
-
+-- testing Matrice
 matTest = GMatrice $ (Line [3.0, 1, 4, -1] 7.0):(Line [2.0, -2, -1, 2] 1):(Line [5.0, 7, 14, -8] 20):(Line [1.0, 3, 2, 4] (-4.0)):[]
