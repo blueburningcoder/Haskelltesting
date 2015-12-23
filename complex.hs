a217b56d8e6e805c1cb2a28d9e55efb4293cf45c
blueburningcoder <f.karg10@gmail.com>
2015-12-23 09:02:10 +0100

    Added infix-versions of most operations

--- complex.hs
+++ complex.hs
@@ -10,35 +10,64 @@ instance Show Complex where
     show (Co r i) = show r ++ " " ++ show i ++ "i"
 
 
+-- | converting a Real number to a simple Complex one
 simpleComplex :: Double -> Complex
 simpleComplex d = Co d 0
 
+-- | converting two Real numbers to a Complex one
 toComplex :: Double -> Double -> Complex
 toComplex r i = Co r i
 
-infixl 9 +:
-(+:) :: Double -> Double -> Complex
-r +: i = toComplex r i
+infixl 9 ~:
+(~:) :: Double -> Double -> Complex
+r ~: i = toComplex r i
 
-complexAdd :: Complex -> Complex -> Complex
-complexAdd (Co r i) (Co rr ii) = Co (r + rr) (i + ii)
+-- | adding two Complex numbers together
+addComplex :: Complex -> Complex -> Complex
+addComplex (Co r i) (Co rr ii) = Co (r + rr) (i + ii)
 
-complexMult :: Complex -> Complex -> Complex
-complexMult (Co r i) (Co rr ii) = Co (r * rr + i * ii) (r * i + rr * ii)
+infixl 6 +:
+(+:) :: Complex -> Complex -> Complex
+r +: i = addComplex r i
 
-complexSub :: Complex -> Complex -> Complex
-complexSub (Co r i) (Co rr ii) = Co (r - rr) (i - ii)
+-- | multiplying two Complex numbers together
+multComplex :: Complex -> Complex -> Complex
+multComplex (Co r i) (Co rr ii) = Co (r * rr - i * ii) (r * i + rr * ii)
 
+infixl 7 *:
+(*:) :: Complex -> Complex -> Complex
+r *: i = multComplex r i
+
+-- | subtracting two Complex numbers from one another
+subComplex :: Complex -> Complex -> Complex
+subComplex (Co r i) (Co rr ii) = Co (r - rr) (i - ii)
+
+infixl 6 -:
+(-:) :: Complex -> Complex -> Complex
+r -: i = subComplex r i
+
+-- | conjugating a Complex number
 conjugate :: Complex -> Complex
 conjugate (Co r i) = Co r (-i)
+con = conjugate
 
-complexDiv :: Complex -> Complex -> Complex
-complexDiv _ (Co 0 0) = error "can not divide through zero"
-complexDiv e@(Co a b) f@(Co c d) = complexdiv (complexMult e (conjugate f)) (c^2 + d^2)
+-- | dividing two Complex numbers through one another
+divComplex :: Complex -> Complex -> Complex
+divComplex _ (Co 0 0) = error "can not divide through zero"
+divComplex e@(Co a b) f@(Co c d) =  (e *: (con f)) /- (c^2 + d^2)
 
+infixl 7 /:
+(/:) :: Complex -> Complex -> Complex
+r /: i = divComplex r i
 
-complexdiv :: Complex -> R -> Complex
-complexdiv (Co a b) t = Co (a/t) (b/t)
+-- | dividing a Complex number through a Real one
+divcomplex :: Complex -> R -> Complex
+divcomplex (Co a b) t = Co (a/t) (b/t)
+
+infixl 7 /-
+(/-) :: Complex -> Double -> Complex
+i /- r = divcomplex i r
+
 
 
 
