9dfb64ba7779378c0e5f35b29d929f41cb319e85
blueburningcoder <f.karg10@gmail.com>
2016-02-04 16:31:36 +0100

    Finished Binary instances

--- change_anime.hs
+++ change_anime.hs
@@ -2,6 +2,7 @@
 
 import Data.Binary
 
+-- All kinda genres that an anime might or might not have
 data Genres = Action | Adventure | Comedy | Crime | Fantasy | Fiction | Historical | Horror | Magical 
         | Mystery | Paranoid | Philosophical | Political | Romance | Saga | Satire | ScienceFiction 
         | SliceOfLife | Speculative | Thriller | Urban | Western
@@ -13,6 +14,7 @@ instance Binary Genres where
     get = do gen <- get
              return $ read gen
 
+-- A Rating usually consists of a list of genres and a list of their individual scores, 1-10 respectively
 data Rating = None | Rating [Genres] [Int] Double
     deriving (Read, Eq)
 
@@ -32,17 +34,45 @@ instance Binary Rating where
                 rat <- get
                 ov  <- get
                 return (Rating gen rat ov)
+
+-- currnetly only showing the overall Rating if necessary
 instance Show Rating where
     show None = "•not available•"
     show (Rating _ _ o) = show o
 
-
+-- The number of Episodes is either Unknown, Zero (yet), or some Int
 data Episodes = Unknown | Zero | Int
     deriving (Show, Read, Eq, Ord)
+
+instance Binary Episodes where
+    put Unknown = put (-1 :: Int)
+    put Zero    = put (0 :: Int)
+    put num     = put (1 :: Int) >> put num
+
+    get = do t <- get :: Get Int
+             case t of
+              (-1) -> return Unknown
+              0 -> return Zero
+              1 -> do num <- get; return num
 
 type WatchedE = Episodes
 
 data Anime = Anime String Rating Episodes WatchedE
+
+instance Binary Anime where
+    put (Anime nam rat ep wa) = do
+        put nam
+        put rat
+        put ep
+        put wa
+    get = do
+        nam <- get
+        rat <- get
+        ep <- get
+        wa <- get
+        return $ Anime nam rat ep wa
+
+
 
 type AllAnime = [Anime]
 type Watched  = [Anime]
@@ -52,10 +82,11 @@ instance Show Anime where
     show (Anime name rat ep wa) = "Anime: " ++ show name ++ " with rating: " ++ show rat ++ " and " ++ show wa ++ "/" ++ show ep ++ " seen."
 -- "
 
-{-
-loadList :: AllAnime
+saveList :: AllAnime -> IO ()
+saveList list = encodeFile "anime.txt" list
+
+loadList :: IO AllAnime
 loadList = decodeFile "anime.txt"
--}
 
 newAnime :: String -> Anime
 newAnime name = Anime name None Unknown Unknown
