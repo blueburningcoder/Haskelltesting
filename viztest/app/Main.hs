84fbd072ef714238cf5ddb8610d324275cb9a77a
blueburningcoder <f.karg10@gmail.com>
2016-02-18 20:20:41 +0100

    Added HsOpenGL-Testing

---
+++ viztest/app/Main.hs
@@ -0,0 +1,66 @@
+module Main where
+
+import Lib
+import Graphics.UI.GLUT
+ 
+
+
+myPoints :: [(GLfloat,GLfloat,GLfloat)]
+myPoints = [ (sin (2*pi*k/12), cos (2*pi*k/12), 0) | k <- [1..12] ]
+
+
+main :: IO ()
+main = do
+  (_progName, _args) <- getArgsAndInitialize
+  _window <- createWindow "Hello World"
+  displayCallback $= display
+  mainLoop
+
+
+-- for some reason, the Triangles get pink the moment the size of the window is changed
+display :: DisplayCallback
+display = do 
+
+  clear [ColorBuffer]
+  let color3f r g b = color $ Color3 r g (b :: GLfloat)
+      vertex3f x y z = vertex $ Vertex3 x y (z :: GLfloat)
+
+  renderPrimitive Quads $ do
+    color3f 1 0 0
+    vertex3f 0 0 0
+    vertex3f 0 0.2 0
+    vertex3f 0.2 0.2 0
+    vertex3f 0.2 0 0
+
+    color3f 0 1 0
+    vertex3f 0 0 0
+    vertex3f 0 (-0.2) 0
+    vertex3f 0.2 (-0.2) 0
+    vertex3f 0.2 0 0
+
+    color3f 0 0 1
+    vertex3f 0 0 0
+    vertex3f 0 (-0.2) 0
+    vertex3f (-0.2) (-0.2) 0
+    vertex3f (-0.2) 0 0
+
+    color3f 1 0 1
+    vertex3f 0 0 0
+    vertex3f 0 0.2 0
+    vertex3f (-0.2) 0.2 0
+    vertex3f (-0.2) 0 0
+  flush
+
+  clear [ColorBuffer]
+  renderPrimitive Triangles $
+    mapM_ (\(x, y, z) -> vertex $ Vertex3 x y z) myPoints
+  flush
+
+
+
+
+
+
+
+
+
