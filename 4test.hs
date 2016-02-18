d2bbd0bee86086d043c872385c14e5015a3805ae
blueburningcoder <f.karg10@gmail.com>
2016-02-18 07:13:53 +0100

    The List as Monad

--- 4test.hs
+++ 4test.hs
@@ -1,3 +1,10 @@
+
+import Control.Applicative
+import Control.Monad
+import Control.Monad.Writer
+
+
+
 -- returns the biggest divisor of both numbers, a > b does not matter
 euklidian :: Integral a => a -> a -> a
 euklidian a 0 = a
@@ -37,3 +44,60 @@ anti (a:b) (c:d) = if a == c then anti b d else c:(anti (a:b) d)
 φ' :: Int -> [Int]
 φ' a = filter (\n -> euklidian a n == 1) $ [1..a]
 --
+
+
+
+
+
+
+
+--         some more experimenting
+
+
+
+
+type KnightPos = (Int, Int)
+
+d :: KnightPos -> KnightPos -> Double
+d (a,b) (c,d) = sqrt . fromIntegral $ ((c - a) ^ 2 + (d - b) ^ 2)
+
+
+moveKnight :: KnightPos -> [KnightPos]
+moveKnight (c, r) = [(c', r') | c' <- possible <*> [c], r' <- possible <*> [r], d (c, r) (c', r') == sqrt 5 && isElem c' && isElem r']
+    where possible = [(+), flip (-)] <*> [1,2]
+          isElem = flip elem $ [1..8]
+
+
+in3 :: KnightPos -> [KnightPos]
+in3 start = return start >>= moveKnight >>= moveKnight >>= moveKnight
+
+
+canReachIn3 :: KnightPos -> KnightPos -> Bool
+canReachIn3 pos1 pos2 = pos2 `elem` in3 pos1
+
+
+canReachIn :: Int -> KnightPos -> KnightPos -> [[KnightPos]]
+canReachIn _ from aim | from == aim = return . return $ aim
+canReachIn 0 _ _ = [[]]
+canReachIn n from aim = filter (not . null) $ moveKnight from >>= reach >>= return . (from:) >>= return . (\p -> if last p == aim then p else [])
+    where reach = flip (canReachIn (n-1)) $ aim
+
+
+
+
+gcd' :: Int -> Int -> Writer [String] Int
+gcd' a b
+    | b == 0 = do
+        tell ["Finished with " ++ show a]
+        return a
+    | otherwise = do
+        tell [show a ++ " % " ++ show b ++ " = " ++ show (mod a b)]
+        gcd' b (mod a b)
+
+-- getting only the answer: fst $ runWriter (gcd' n1 n2)
+-- getting only the log: mapM_ putStrLn $ snd $ runWriter (gcd' n1 n2)
+
+
+
+
+
