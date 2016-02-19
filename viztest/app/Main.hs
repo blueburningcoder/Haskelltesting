649df8b2eccf5ddce1fe206964de8fa2daabe537
blueburningcoder <f.karg10@gmail.com>
2016-02-19 16:59:04 +0100

    Supposed to rotate in a nice way

--- viztest/app/Main.hs
+++ viztest/app/Main.hs
@@ -1,62 +1,22 @@
 module Main where
 
-import Lib
+import Bindings
+import Display
 import Graphics.UI.GLUT
- 
-
-
-myPoints :: [(GLfloat,GLfloat,GLfloat)]
-myPoints = [ (sin (2*pi*k/12), cos (2*pi*k/12), 0) | k <- [1..12] ]
+import Data.IORef 
 
 
 main :: IO ()
 main = do
   (_progName, _args) <- getArgsAndInitialize
+  initialDisplayMode $= [DoubleBuffered]
   _window <- createWindow "Hello World"
-  displayCallback $= display
+  reshapeCallback $= Just reshape
+  keyboardMouseCallback $= Just keyboardMouse
+  angle <- newIORef 0.0
+  displayCallback $= display angle
+  idleCallback $= Just (idle angle)
   mainLoop
-
-
--- for some reason, the Triangles get pink the moment the size of the window is changed
-display :: DisplayCallback
-display = do 
-
-  clear [ColorBuffer]
-  let color3f r g b = color $ Color3 r g (b :: GLfloat)
-      vertex3f x y z = vertex $ Vertex3 x y (z :: GLfloat)
-
-  renderPrimitive Quads $ do
-    color3f 1 0 0
-    vertex3f 0 0 0
-    vertex3f 0 0.2 0
-    vertex3f 0.2 0.2 0
-    vertex3f 0.2 0 0
-
-    color3f 0 1 0
-    vertex3f 0 0 0
-    vertex3f 0 (-0.2) 0
-    vertex3f 0.2 (-0.2) 0
-    vertex3f 0.2 0 0
-
-    color3f 0 0 1
-    vertex3f 0 0 0
-    vertex3f 0 (-0.2) 0
-    vertex3f (-0.2) (-0.2) 0
-    vertex3f (-0.2) 0 0
-
-    color3f 1 0 1
-    vertex3f 0 0 0
-    vertex3f 0 0.2 0
-    vertex3f (-0.2) 0.2 0
-    vertex3f (-0.2) 0 0
-  flush
-
-  clear [ColorBuffer]
-  renderPrimitive Triangles $
-    mapM_ (\(x, y, z) -> vertex $ Vertex3 x y z) myPoints
-  flush
-
-
 
 
 
---
+++ viztest/src/Cube.hs
@@ -0,0 +1,22 @@
+module Cube where
+
+import Graphics.UI.GLUT
+
+vertex3f :: (GLfloat, GLfloat, GLfloat) -> IO ()
+vertex3f (x,y,z) = vertex $ Vertex3 x y z
+
+
+
+cube :: GLfloat -> IO ()
+cube w = do
+  renderPrimitive Quads $ mapM_ vertex3f 
+    [ ( w, w, w), ( w, w,-w), ( w,-w,-w), ( w,-w, w),
+      ( w, w, w), ( w, w,-w), (-w, w,-w), (-w, w, w),
+      ( w, w, w), ( w,-w, w), (-w,-w, w), (-w, w, w),
+      (-w, w, w), (-w, w,-w), (-w,-w,-w), (-w,-w, w),
+      ( w,-w, w), ( w,-w,-w), (-w,-w,-w), (-w,-w, w),
+      ( w, w,-w), ( w,-w,-w), (-w,-w,-w), (-w, w,-w) ]
+
+
+
+
